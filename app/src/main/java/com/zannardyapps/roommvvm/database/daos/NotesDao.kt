16812b504554ef1aeb771bb5efac9e45446e5c9b
Zannardy Andrade Oliveira <78097970+ZannF0x@users.noreply.github.com>
2021-10-27 16:08:06 -0300

    Implementação - NotesRepository

--- app/src/main/java/com/zannardyapps/roommvvm/database/daos/NotesDao.kt
+++ app/src/main/java/com/zannardyapps/roommvvm/database/daos/NotesDao.kt
@@ -5,12 +5,13 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.zannardyapps.roommvvm.database.model.Notes
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NotesDao {
 
     @Query("SELECT * FROM notes_table ORDER BY notes_id ASC")
-    fun getNotes(): List<Notes>
+    fun getNotes(): Flow<List<Notes>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(notes: Notes)
---
+++ app/src/main/java/com/zannardyapps/roommvvm/repository/NotesRepository.kt
@@ -0,0 +1,17 @@
+package com.zannardyapps.roommvvm.repository
+
+import androidx.annotation.WorkerThread
+import com.zannardyapps.roommvvm.database.daos.NotesDao
+import com.zannardyapps.roommvvm.database.model.Notes
+import kotlinx.coroutines.flow.Flow
+
+class NotesRepository(private val notesDao: NotesDao) {
+
+    val allNotes: Flow<List<Notes>> = notesDao.getNotes()
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insert(notes: Notes){
+        notesDao.insert(notes)
+    }
+}
