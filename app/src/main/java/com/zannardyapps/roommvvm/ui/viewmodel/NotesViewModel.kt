309a94e34c5520a737d2a13e8427f42cf9c99d5f
Zannardy Andrade Oliveira <78097970+ZannF0x@users.noreply.github.com>
2021-10-27 16:48:28 -0300

    Implementação - NotesViewModel / Cores / Layouts

---
+++ app/src/main/java/com/zannardyapps/roommvvm/ui/viewmodel/NotesViewModel.kt
@@ -0,0 +1,26 @@
+package com.zannardyapps.roommvvm.ui.viewmodel
+
+import androidx.lifecycle.*
+import com.zannardyapps.roommvvm.database.model.Notes
+import com.zannardyapps.roommvvm.repository.NotesRepository
+import kotlinx.coroutines.launch
+
+class NotesViewModel(private val repository: NotesRepository): ViewModel() {
+
+    val allNotesLiveData: LiveData<List<Notes>> = repository.allNotes.asLiveData()
+
+    fun insert(notes: Notes) = viewModelScope.launch {
+        repository.insert(notes)
+    }
+
+    inner class NotesViewModelFactory(private val repository: NotesRepository) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(NotesViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return NotesViewModel(repository) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
+
+    }
+}
